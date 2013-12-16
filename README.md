27b8d59fb012bfb0353dd2baaca8e658a81259ce
Tom MacWright <tom@macwright.org>
2013-12-15 20:33:12 -0500

    readme+tests

--- README.md
+++ README.md
@@ -2,15 +2,23 @@
 
 # geojson-area
 
-Calculate the area inside of a GeoJSON Polygon.
+Calculate the area inside of a [GeoJSON](http://geojson.org/) Polygon.
 
 ## usage
 
     npm install geojson-area
 
+## example
+
+```js
+var geojsonArea = require('geojson-area');
+
+var area = geojsonArea.geometry(obj);
+```
+
 ## api
 
-### `area(obj)`
+### `geojsonArea.geometry(obj)`
 
 Given a Geometry object of type Polygon or MultiPolygon, return contained
 area as square meters. Invalid input will return `null`.
--- test/basic.js
+++ test/basic.js
@@ -4,11 +4,11 @@ var gjArea = require('../'),
 describe('geojson area', function() {
     it('computes the area of illinois', function() {
         var ill = require('./illinois.json');
-        assert.equal(gjArea(ill), 145978332359.37125);
+        assert.equal(gjArea.geometry(ill), 145978332359.37125);
     });
     // http://www.wolframalpha.com/input/?i=surface+area+of+earth
     it('computes the area of the world', function() {
         var all = require('./all.json');
-        assert.equal(gjArea(all), 511207893395811.06);
+        assert.equal(gjArea.geometry(all), 511207893395811.06);
     });
 });
