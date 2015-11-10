b1793229dc7159e923e1f1f53f9ab715ebee3417
Spencer Eldred <spencer@konturlabs.com>
2015-11-09 15:32:44 -1000

    revise implementation of algorithm to calculate area of polygon on a sphere

--- index.js
+++ index.js
@@ -53,14 +53,29 @@ function polygonArea(coords) {
  */
 
 function ringArea(coords) {
-    var area = 0;
+    var p1, p2, p3, lowerIndex, middleIndex, upperIndex,
+    area = 0,
+    coordsLength = coords.length;
 
-    if (coords.length > 2) {
-        var p1, p2;
-        for (var i = 0; i < coords.length - 1; i++) {
-            p1 = coords[i];
-            p2 = coords[i + 1];
-            area += rad(p2[0] - p1[0]) * (2 + Math.sin(rad(p1[1])) + Math.sin(rad(p2[1])));
+    if (coordsLength > 2) {
+        for (i = 0; i < coordsLength; i++) {
+            if (i === coordsLength - 2) {// i = N-2
+                lowerIndex = coordsLength - 2;
+                middleIndex = coordsLength -1;
+                upperIndex = 0;
+            } else if (i === coordsLength - 1) {// i = N-1
+                lowerIndex = coordsLength - 1;
+                middleIndex = 0;
+                upperIndex = 1;
+            } else { // i = 0 to N-3
+                lowerIndex = i;
+                middleIndex = i+1;
+                upperIndex = i+2;
+            }
+            p1 = coords[lowerIndex];
+            p2 = coords[middleIndex];
+            p3 = coords[upperIndex];
+            area += ( rad(p3[0]) - rad(p1[0]) ) * Math.sin( rad(p2[1]));
         }
 
         area = area * wgs84.RADIUS * wgs84.RADIUS / 2;
